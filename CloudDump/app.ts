91ae187a8727c4e56456741a0437aa344e24823b
Costanza Laudisa <c.laudisa@students.uu.nl>
2023-05-24 15:49:45 +0200

    Add project files.

---
+++ CloudDump/app.ts
@@ -0,0 +1,14 @@
+const express = require('express');
+
+const app = express();
+const port = 1337;
+
+// Serve the HTML file
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/login.html');
+});
+
+// Start the server
+app.listen(port, () => {
+    console.log('Server started on port ' + port);
+});
