834529f8cc8e5188b33ad70fdceb07f6ab9235be
Deep Blue K <119618811+Yun-In-Info@users.noreply.github.com>
2023-05-25 15:28:08 +0200

    Add files via upload

---
+++ CloudDump/homepage.js
@@ -0,0 +1,14 @@
+var downloadButton = document.querySelector('.download-button');
+var shareButton = document.querySelector('.share-button');
+var tagsButton = document.querySelector('.tags-button');
+
+downloadButton.addEventListener('click', function() {
+// Code to handle the download button click
+});
+
+shareButton.addEventListener('click', function() {
+// Code to handle the share button click
+});
+
+tagsButton.addEventListener('click', function() {
+});
